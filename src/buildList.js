927279a33d49c36482fdbc9dc3b121fb1e5e8342
goodogfun <goodogfun@gmail.com>
2025-07-19 17:36:20 +0800

    support token tags

--- src/buildList.js
+++ src/buildList.js
@@ -16,14 +16,18 @@ module.exports = function buildList() {
       minor: +parsed[1],
       patch: +parsed[2],
     },
-    // tags: {
-    //     "GooDog": {
-    //         "name": "GooDog Supported Token List",
-    //         "description": "GooDog Supported Token List",
-    //     }
-    // },
+    tags: {
+        "GooDog": {
+            "name": "GooDog",
+            "description": "Built for Good, Made for Fun",
+        },
+        "GPAD": {
+            "name": "GPAD",
+            "description": "GPAD Token Activated by Community",
+        }
+    },
     logoURI: "https://gist.githubusercontent.com/GooDogFun/e9d02c4a02d24d8642ac813fc14ff6fc/raw/6b0e7ad50c3e9c3c0be149cfa0e3752c9dc2b065/GD.svg",
-    // keywords: ["GooDog"],
+    keywords: ["GooDog", "Good OG", "Community", "GooDog Protocol"],
     tokens: [...mainnet, ...optimismSepolia, ...dbk]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
